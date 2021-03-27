caa6a368099e07ec1b939c71a79a44045f1340d6
shengda567 <57602285+shengda567@users.noreply.github.com>
2021-03-26 22:03:51 -0400

    first commit

---
+++ ShengdaConsoleApplication/ShengdaConsoleApplication/PersonInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ShengdaConsoleApplication
+{
+    public class PersonInfo
+    {
+        string FirstName;
+        string LastName;
+
+        public PersonInfo(String FName, String LName)
+        {
+            FirstName = FName;
+            LastName = LName;
+        }
+
+        public void PresentToScreen()
+        {
+            Console.WriteLine(this.FirstName + this.LastName);
+        }
+
+        public void WriteToTextFile()
+        {
+            string writeText = $"First Name is : {this.FirstName}, Last Name is {this.LastName}";
+            File.WriteAllText("FirstName_LastName_Information.txt", writeText);
+        }
+
+        public void ReadTextToScreen()
+        {
+            string readText = File.ReadAllText("FirstName_LastName_Information.txt");  // Read the contents of the file
+            Console.WriteLine(readText);  // Output the content
+        }
+    }
+}
---
+++ ShengdaConsoleApplication/ShengdaConsoleApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ShengdaConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enhance the application to prompt the user for their name and display it along with the date and time
+            //Console.WriteLine("What is your name?");
+            //var name = Console.ReadLine();
+            //var date = DateTime.Now;
+            //Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {date:d} at {date:t}!");
+            //Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+            //Console.ReadKey(true);
+
+            Console.WriteLine("Here is executing the PersonInfo class");
+            PersonInfo pi = new PersonInfo("Shengda", "Zhang");
+            pi.PresentToScreen();
+            pi.WriteToTextFile();
+            pi.ReadTextToScreen();
+
+            Console.WriteLine("Here the PersonInfo class is done ");
+        }
+    }
+}
